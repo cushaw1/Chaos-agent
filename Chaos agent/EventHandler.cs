86ec2334e9deb547a7d15939177a300aefecc1c3
cushaw1 <43043675+cushaw1@users.noreply.github.com>
2018-09-11 21:42:20 +0800

    Update EventHandler.cs

--- Chaos agent/EventHandler.cs
+++ Chaos agent/EventHandler.cs
@@ -90,7 +90,6 @@ namespace Chaos_agent_plugin
                     ev.Player.SetHealth(heal, DamageType.NONE);
                     Chaosid = null;
                     players.Clear();
-                    System.Threading.Thread.Sleep(1000);
                     foreach (int id in reward)
                     {
                         ev.Player.GiveItem((ItemType)id);
